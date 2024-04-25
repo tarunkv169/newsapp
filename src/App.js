2f96bd6f64c0dd84af1242c1ae06766a57c2756a
Tarunkumar-33 <125637587+Tarunkumar-33@users.noreply.github.com>
2024-04-25 16:20:46 +0530

    dark-mode

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import News  from "./components/News";
 import { BrowserRouter as Router,Routes, Route, } from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
+// import Newsitems from "./components/Newsitems";
 
 
 export default function App(){
@@ -15,31 +16,47 @@ export default function App(){
   // const state={progress: 0}      // 2. var progress is used anywhere to assign value
 
   const [progress,setProgress]=useState(0);
-  const setprogress=(progress)=>{
-    setProgress(progress)  // 3. we create func to manipulate var progress value
-  }                                      // 4. passed this func in Route to News.js  where it is used to manipulate value by passing arguments
+  // const setProgress=(progress)=>{
+  //   setProgress(progress)  // 3. we create func to manipulate var progress value
+  // }                                      // 4. passed this func in Route to News.js  where it is used to manipulate value by passing arguments
 
+  
+  const [mode,setMode] = useState('light');
+  
+  const togglemode=()=>{
+    if(mode==='dark')
+    {                                               // always remmeber while func call onclick={---},style={mystyle} etc 
+      setMode('light'); 
+      document.body.style.backgroundColor='white'; 
+      // showalert(" Dark mode disabled","success");                            // for setmode('---')
+    }
+    else{
+      setMode('dark');
+      document.body.style.backgroundColor='black';                               // for setmode('---')
+      // showalert(" Dark mode enabled","success");                            // for setmode('---')
+    }
+   }
 
   
     return (
       <>
       <div>
         <Router>
-          <Navbar />  
+          <Navbar  mode={mode} togglemode={togglemode}/> 
           <LoadingBar
            color='#f11946'
            progress={progress}  // 1. we set value of progress using state which defined its value with variable
           />                             
                                               
            <Routes>                               
-              <Route path="/" element={<News setprogress={setprogress} apikey={apikey} key="general" country="in" pageSize={pageSize} category="general" />} />
-              <Route path="/business" element={<News setprogress={setprogress} apikey={apikey} key="business" country="in" pageSize={pageSize} category="business" />} />
-              <Route path="/entertainment" element={<News setprogress={setprogress} apikey={apikey} key="entertainment" country="in" pageSize={pageSize} category="entertainment" />} />
-              <Route path="/general" element={<News setprogress={setprogress} apikey={apikey} key="general" country="in" pageSize={pageSize} category="general" />} />
-              <Route path="/health" element={<News setprogress={setprogress} apikey={apikey} key="health" country="in" pageSize={pageSize} category="health" />} />
-              <Route path="/science" element={<News setprogress={setprogress} apikey={apikey} key="science" country="in" pageSize={pageSize} category="science" />} />
-              <Route path="/sports" element={<News setprogress={setprogress} apikey={apikey} key="sports" country="in" pageSize={pageSize} category="sports" />} />
-              <Route path="/technology" element={<News setprogress={setprogress} apikey={apikey} key="technology" country="in" pageSize={pageSize} category="technology" />} />
+              <Route path="/" element={<News setProgress={setProgress} apikey={apikey} key="general" country="in" pageSize={pageSize} category="general" mode={mode} togglemode={togglemode}/>}   />
+              <Route path="/business" element={<News setProgress={setProgress} apikey={apikey} key="business" country="in" pageSize={pageSize} category="business"   mode={mode} togglemode={togglemode}/>} />
+              <Route path="/entertainment" element={<News setProgress={setProgress} apikey={apikey} key="entertainment" country="in" pageSize={pageSize} category="entertainment"  mode={mode} togglemode={togglemode} />} />
+              <Route path="/general" element={<News setProgress={setProgress} apikey={apikey} key="general" country="in" pageSize={pageSize} category="general"   mode={mode} togglemode={togglemode}/>} />
+              <Route path="/health" element={<News setProgress={setProgress} apikey={apikey} key="health" country="in" pageSize={pageSize} category="health"   mode={mode} togglemode={togglemode}/>} />
+              <Route path="/science" element={<News setProgress={setProgress} apikey={apikey} key="science" country="in" pageSize={pageSize} category="science"   mode={mode} togglemode={togglemode}/>} />
+              <Route path="/sports" element={<News setProgress={setProgress} apikey={apikey} key="sports" country="in" pageSize={pageSize} category="sports"   mode={mode} togglemode={togglemode}/>} />
+              <Route path="/technology" element={<News setProgress={setProgress} apikey={apikey} key="technology" country="in" pageSize={pageSize} category="technology"   mode={mode} togglemode={togglemode}/>} />
            </Routes>
         </Router>
         </div>
